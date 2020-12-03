e8c67e9b80d371271b4830041480a2ac7f153599
Kanashii <marlison.silva@escolar.ifrn.edu.br>
2020-12-03 17:09:38 -0300

    template e algumas rotas implementadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'App\Http\Controllers\web\HomeController@home');
+
+Route::get('/home', function(){
+    return '<h1>Aqui é sua casa!</h1> Você tem uma cozinha no /kitchen , pq não vai dar uma olhada? ';
+});
+Route::get('/home/kitchen', function(){
+    return '<h1>Aqui é sua cozinha</h1> Desculpe, mas só temos esse cômodo na casa, <br>você faz suas necessidades na caixinha de areia no canto da parede, <br>quer ir até lá? Digite /sand.';
+});
+Route::get('/home/kitchen/sand', function(){
+    return '<h1>Aqui é sua caixa de areia</h1> Parece que você passou por aqui recentemente, <br>se eu fosse você iria embora.';
 });
